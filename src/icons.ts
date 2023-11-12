0748af95740b666368c9bfdc676144771616998a
008 <007>
2023-11-13 00:34:58 +0800

    rm icons

--- src/icons.ts
+++ src/icons.ts
@@ -1,13 +1,4 @@
 export const ICONS = `
-<symbol id="iconBooks" viewBox="0 0 32 32"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32">
-<path d="M24.5 4c1.89 0 3.5 1.61 3.5 3.5v18c0 1.89-1.61 3.5-3.5 3.5h-17C2.61 29 1 27.39 1 25.5v-18C1 5.61 2.61 4 4.5 4h20zm0-2h-20C1.12 2 0 3.12 0 4.5v18C0 24.88 1.12 26 2.5 26h17c.28 0 .5-.22.5-.5v-18c0-.28-.22-.5-.5-.5zm-14 22h-3v-2h3v2zm0-4h-3v-2h3v2zm0-4h-3v-2h3v2zm6 8h-3v-2h3v2zm0-4h-3v-2h3v2zm0-4h-3v-2h3v2zm8 8h-3v-2h3v2zm0-4h-3v-2h3v2zm0-4h-3v-2h3v2z" fill="#000"/>
-</svg></symbol>
-
-<symbol id="iconToolbox" viewBox="0 0 32 32"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
-<path d="M28.5 6h-5.7C21.4 4.7 20.2 4 19 4h-6C11.8 4 10.6 4.7 9.2 6H3.5C2.7 6 2 6.7 2 7.5v20C2 28.3 2.7 29 3.5 29h25c.8 0 1.5-.7 1.5-1.5v-20c0-.8-.7-1.5-1.5-1.5zM19 6H13v-.5c0-.8.7-1.5 1.5-1.5h3c.8 0 1.5.7 1.5 1.5V6zm4 22H3V7h3v1.5C6 9.3 6.7 10 7.5 10h1c.8 0 1.5-.7 1.5-1.5V7h6v1.5c0 .8.7 1.5 1.5 1.5h1c.8 0 1.5-.7 1.5-1.5V7h3v21z"/>
-<path d="M8 12h2v2H8zm4 0h2v2h-2zm4 0h2v2h-2zm4 0h2v2h-2zm-12 4h2v2H8zm4 0h2v2h-2zm4 0h2v2h-2zm4 0h2v2h-2zm-12 4h2v2H8zm4 0h2v2h-2zm4 0h2v2h-2zm4 0h2v2h-2z"/>
-</svg></symbol>
-
 <symbol id="iconContents" viewBox="0 0 32 32"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
 <path d="M4 6h24v4H4zm0 6h24v4H4zm0 6h24v4H4zm0 6h24v4H4z" fill="#000" />
 </svg></symbol>
